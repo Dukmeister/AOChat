325e831dbc2be2b91429a15745ec44c5db57e87b
Dukmeister <hz_ducky@hotmail.com>
2012-07-28 10:36:10 -0400

    Initial Commit

---
+++ src/main/java/net/aocraft/plugins/AOChat/App.java
@@ -0,0 +1,13 @@
+package net.aocraft.plugins.AOChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
